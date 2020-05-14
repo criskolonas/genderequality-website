1d8dcbdacdd9e0b8702b7b575bb4da8c6769381b
Chris Kolonas <ckolonas@csd.auth.gr>
2020-05-14 19:05:11 +0300

    added stuff

---
+++ php/login.php
@@ -0,0 +1,6 @@
+<?php
+//get values passed from login form
+  $username = $POST['usrid'];
+  $password = $POST['passwrd'];
+
+ ?>
---
+++ php/register.php
@@ -0,0 +1,7 @@
+<?php
+//get values passed from register form
+  $username = $POST['regid'];
+  $password = $POST['regpass'];
+  $email = $POST['regemail'];
+
+ ?>

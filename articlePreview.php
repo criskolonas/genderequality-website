f1aa25e2cff41796b5d92b2ccaf6cf4bf51e6f34
Chris Kolonas <ckolonas@csd.auth.gr>
2020-05-23 17:17:52 +0300

    created article preview

---
+++ articlePreview.php
@@ -0,0 +1,17 @@
+
+ <?php
+ include 'php/connection.php';
+ $id = $_GET['id'];
+ $sql = "SELECT * FROM articles WHERE ArticleId=$id";
+ $result = mysqli_query($link,$sql);
+ $row= mysqli_fetch_array($result);
+ $articleId =$row['ArticleId'];
+ $articleName=$row['ArticleName'];
+ $articleContent=$row['ArticleContent'];
+ $articleImage = $row['ArticleImage'];
+ $articleDate = $row['ArticleDate'];
+ echo json_encode(array("articleName"=>$articleName,"articleImage"=>$articleImage,"articleContent"=>$articleContent));
+
+
+ mysqli_close($link);
+ ?>

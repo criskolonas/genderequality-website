9e3fe524a5997bb3ea57ac732d98206c0a362c26
Chris Kolonas <ckolonas@csd.auth.gr>
2020-05-23 20:25:06 +0300

    article update final

--- articleUpdate.php
+++ articleUpdate.php
@@ -1,9 +1,11 @@
 <?php
 include 'php/connection.php';
-$id = $_GET['id'];
-$name= $_GET['name'];
-$content = $_get['content'];
-$image = $_get['image'];
-$sql="UPDATE articles SET ArticleName=$name,ArticleContent=$content,ArticleImage=$image WHERE ArticleId=$id ";
-$query=mysqli_query($link,$sql);
+$id = $_GET['update_id'];
+$name=  $_GET['aname'];
+$content =  $_GET['content'];
+$image =  $_GET['image'];
+$sql="UPDATE articles SET articleName='$name',articleContent='$content',articleImage='$image' WHERE articleId=$id ";
+$result=mysqli_query($link,$sql);
+mysqli_close($link);
+header('Location: editcontent.php');
 ?>
